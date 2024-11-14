76eae0724a3e995f1cb4dce595e256aa6099bdc6
copy-ninja1000 <emmajnr1000@gmail.com>
2024-11-14 13:41:45 +0100

    fix(types): export all types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import CountryInfoData from "./countryInfoData";
 import CountryInfoQuery from "./query";
+import {
+  ContinentCode,
+  CountryCode,
+  CountryDetails,
+  RegionCode,
+  ContinentName,
+} from "./types";
 
 export { CountryInfoQuery };
 
 export default CountryInfoData;
+export type {
+  ContinentCode,
+  CountryCode,
+  CountryDetails,
+  RegionCode,
+  ContinentName,
+};
