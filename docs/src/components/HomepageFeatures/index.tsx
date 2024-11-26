3904002aced767ad988d9bb2dc5a8d51c1ba92d5
sammy-code98 <sammysmart98@yahoo.com>
2024-11-26 16:54:07 +0100

    feat:home page

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,69 +1,69 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+  id: number;
+  text: string;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  { id: 100, text: "Lookup countries by their country code or name." },
+  { id: 101, text: "Get lists of all countries, continents, or regions." },
+  { id: 102, text: "Retrieve detailed country information, including continent and region" },
+  { id: 103, text: "Update country data with new information." },
+  { id: 104, text: "Search for countries based on partial name matches." },
+  { id: 105, text: "Retrieve countries from multiple continents or regions at once." },
+  { id: 106, text: "Check if a country belongs to a specific continent or region." },
+  { id: 107, text: "Filter countries by continent, region, or country code." },
+  { id: 108, text: "Exclude countries by continent, region, country code, or country name." },
+  { id: 109, text: "Filter countries by name or partial name match." },
+  { id: 110, text: "Sort the results by name, continent, or region." },
+  { id: 111, text: "Limit the number of results returned." },
+  { id: 112, text: "Retrieve specific fields from the country details." },
+  { id: 113, text: "Include additional details such as continent and region for each country." },
+  { id: 114, text: "Chain methods for building complex queries." },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+
+function FeatText({ title }: { title: string }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={styles.featContent}>
+      <img src='./img/check.svg' />
+      <span>{title}</span>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        {/* <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div> */}
+        <div>
+          <h1 className={styles.heading}>Country-Info-Data</h1>
+          <p className={styles.text}>The library that provides you with a streamlined solution for managing and retrieving detailed information about continents and countries, including their names, codes, and other associated data, enabling efficient access and organization of geographical details.</p>
+
+          <div className={styles.buttons}>
+            <Link className={styles.button_89} to="/docs/intro">
+              Read The Docs
+            </Link>
+          </div>
+
+          <div className={styles.featContainer}>
+            <h2>Zero dependencies. Full features included.</h2>
+            <p>With zero dependencies, Country-Info-Data is exceptionally lightweight while offering a rich set of features. Whether it's a side project or a large-scale application, the library is a robust tool designed to handle diverse use cases, supporting your projects as they scale with the speed of your creativity.</p>
+          </div>
+
+
+          <div className={styles.gridContainer}>
+            <div className={styles.featGrid}>
+              {FeatureList.map((feat) => (
+                <div key={feat.id}>
+                  <FeatText title={feat.text} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
